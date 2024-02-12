e8bd51dcd95a6b61775dd03594f6b73eeaef7112
PavanKumar <124707043+Pavankurapati03@users.noreply.github.com>
2024-02-12 17:35:57 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 import streamlit as st
 
 
-loadedmodel = pickle.load(open('C:/Users\Pavan/Downloads/Diabetes/trained_model.sav', 'rb'))
+loadedmodel = pickle.load(open('C:/Users/Pavan/Downloads/Diabetes/trained_model.sav', 'rb'))
 
 # creating a function for Prediction
 
